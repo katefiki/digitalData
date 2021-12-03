98bd7050c355daafe95d0fadb11decb63ff3a176
Wilson Ortega <waortegaand@gmail.com>
2021-12-02 22:00:43 -0500

    Sprint3: Edicion modelo Proyecto, se elimina client

---
+++ server/modelos/proyecto/tipos.js
@@ -0,0 +1,66 @@
+import {gql} from 'apollo-server-express';
+
+const proyectoTipos = gpl`
+    input crearObjetivo {
+        descripcion: String!
+        tipo: Enum_TipoObjetivo!
+    }
+
+    input camposObjetivo {
+        descripcion: String!
+        tipo: Enum_TipoObjetivo!
+    }
+
+    input camposProyecto {
+        nombre: String
+        presupuesto: Float
+        fechaInicio: Date
+        fechaFin: Date
+        estado: Enum_EstadoProyecto
+        fase: Enum_FaseProyecto
+        lider: String
+    }
+
+    type Objetivo{
+        _id: ID!
+        descripcion: String!
+        tipo: Enum_TipoObjetivo!
+    }
+
+    type Proyecto{
+        _id: ID!
+        nombre: String!
+        presupuesto: Float!
+        fechaInicio: Date!
+        fechaFin: Date!
+        estado: Enum_EstadoProyecto!
+        fase: Enum_FaseProyecto!
+        lider: Usuario!
+        objetivos: [Objetivo]
+    }
+
+    type Query {
+        Proyectos: [Proyecto]
+    }
+
+    type Mutation {
+      crearProyecto(
+        nombre: String!
+        presupuesto: Float!
+        fechaInicio: Date!
+        fechaFin: Date!
+        estado: Enum_EstadoProyecto!
+        fase: Enum_FaseProyecto!
+        lider: String!
+        objetivos: [crearObjetivo]
+      ): Proyecto
+    editarProyecto(_id: String!, campos: camposProyecto!): Proyecto
+    crearObjetivo(idProyecto: String!, campos: camposObjetivo!): Proyecto
+    editarObjetivo(idProyecto: String!, indexObjetivo: Int!, campos: camposObjetivo!): Proyecto
+    eliminarObjetivo(idProyecto: String!, idObjetivo: String!): Proyecto
+    }
+`;
+
+// Verificar Linea 38 --->  lider: Usuario!
+
+export {proyectoTipos};
