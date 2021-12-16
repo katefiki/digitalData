12a64f6e7b7fb9f8320300fb47a90d55443ed7a0
eilleenmaria <egutierrezacosta85@gmail.com>
2021-12-15 21:52:27 -0500

    canbios en  resolvers y tipos inscripciones

--- server/modelos/inscripcion/resolvers.js
+++ server/modelos/inscripcion/resolvers.js
@@ -1,23 +1,46 @@
 import { InscripcionModel } from './inscripcion.js';
 
 const resolverInscripciones = {
+  Inscripcion: {
+    proyecto: async (parent, args, context) => {
+      return await ProjectModel.findOne({ _id: parent.proyecto });
+    },
+    estudiante: async (parent, args, context) => {
+      return await UserModel.findOne({ _id: parent.estudiante });
+    },
+  },
   Query: {
-    Inscripciones: async (parent, args) => {
-      const inscripciones = await InscripcionModel.find();
+    Inscripciones: async (parent, args, context) => {
+      let filtro = {};
+      if (context.userData) {
+        if (context.userData.rol === 'LIDER') {
+          const projects = await ProjectModel.find({ lider: context.userData._id });
+          const projectList = projects.map((p) => p._id.toString());
+          filtro = {
+            proyecto: {
+              $in: projectList,
+            },
+          };
+        }
+      }
+      const inscripciones = await InscriptionModel.find({ ...filtro });
       return inscripciones;
     },
+
+    // inscripcionesNoAprobadas: async () => {
+    //   const ina = await InscriptionModel.find({ estado: 'PENDIENTE' }).populate('estudiante');
+    // },
   },
   Mutation: {
     crearInscripcion: async (parent, args) => {
-      const inscripcionCreada = await InscripcionModel.create({
-        estado: args.estado,
+      const inscripcionCreada = await InscriptionModel.create({
         proyecto: args.proyecto,
         estudiante: args.estudiante,
       });
       return inscripcionCreada;
     },
     aprobarInscripcion: async (parent, args) => {
-      const inscripcionAprobada = await InscripcionModel.findByIdAndUpdate(
+      const inscripcionAprobada = await InscriptionModel.findByIdAndUpdate(
         args.id,
         {
           estado: 'ACEPTADO',
